1dd58647bdf7b965bb2ce53c62697541486fc2ee
Tohir Raihan <tohir.raihan@gmail.com>
2021-12-17 02:43:17 +0600

    checkUser method added in User class

--- private/classes/app/User.php
+++ private/classes/app/User.php
@@ -17,6 +17,13 @@ class User extends Database
         $sql = "INSERT INTO `users`(`name`, `email`, `password`) VALUES (?,?,?)";
         return parent::insertRow($sql, [$name, $email, $password]);
     }
+
+    // check if a user exists with an email
+    public static function checkUser($email)
+    {
+        $sql = "SELECT * FROM `users` WHERE `email` = ? LIMIT 1";
+        return parent::getRow($sql, [$email]);
+    }
 }
 // Create a new user to instantiate a Connection
 $user = new User;
