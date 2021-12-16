83356c66d98a015456f3c5fd496aede1944bee72
Tohir Raihan <tohir.raihan@gmail.com>
2021-12-16 23:20:23 +0600

    login page design complete

--- private/shared/header.php
+++ private/shared/header.php
@@ -1,13 +1,17 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- font awesome -->
+    <script src="https://kit.fontawesome.com/652ace3a30.js" crossorigin="anonymous"></script>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?=url_for('assets/css/bootstrap.min.css')?>" />
+    <link rel="stylesheet" href="<?= url_for('assets/css/bootstrap.min.css') ?>" />
 
     <title>Coding Task</title>
 </head>
+
 <body>
---
+++ public/dashboard/login.php
@@ -0,0 +1,38 @@
+<?php require_once('../../private/initialize.php'); ?>
+
+<!-- #####=START Header=##### -->
+<?php require_once(SHARED_PATH . '/header.php'); ?>
+<!-- #####=END Header=##### -->
+
+<!-- main content -->
+<div class="container pt-sm-5 mt-5">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-6 col-lg-5 col-xl-4">
+            <div class="card shadow-lg">
+                <div class="border-top border-3 border-primary rounded-top"></div>
+                <i class="d-block fas fa-user-circle text-center text-primary my-3" style="font-size: 80px;"></i>
+
+                <div class="card-body px-5">
+                    <form>
+                        <div class="mb-3">
+                            <input type="email" class="form-control py-2" placeholder="Your email">
+                        </div>
+
+                        <div class="mb-3">
+                            <input type="password" class="form-control py-2" placeholder="Password">
+                        </div>
+                        <button type="submit" class="btn btn-primary d-block col-5 mx-auto my-3">Login</button>
+                    </form>
+                </div>
+                <div class="card-footer text-center py-3">
+                    <a href="#">Don't have an account yet?</a>
+                </div>
+            </div>
+            <!-- /.card -->
+        </div>
+    </div>
+</div>
+
+<!-- #####=START FOOTER=##### -->
+<?php require_once(SHARED_PATH . '/footer.php'); ?>
+<!-- #####=END FOOTER=##### -->
