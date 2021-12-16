e7e8c6724c87ca9bfc3c5844335433d9d91cbd5b
Tohir Raihan <tohir.raihan@gmail.com>
2021-12-16 23:44:40 +0600

    register page design completes

--- public/dashboard/register.php
+++ public/dashboard/register.php
@@ -5,9 +5,9 @@
 <!-- #####=END Header=##### -->
 
 <!-- main content -->
-<div class="container pt-sm-5 mt-5">
+<div class="container pt-sm-5 ">
     <div class="row justify-content-center">
-        <div class="col-12 col-md-6 col-lg-5 col-xl-4">
+        <div class="col-12 col-md-6 col-lg-5">
             <div class="card shadow-lg">
                 <div class="border-top border-3 border-primary rounded-top"></div>
                 <i class="d-block fas fa-user-circle text-center text-primary my-3" style="font-size: 80px;"></i>
@@ -15,17 +15,25 @@
                 <div class="card-body px-5">
                     <form>
                         <div class="mb-3">
+                            <input type="text" class="form-control py-2" placeholder="Your full name">
+                        </div>
+
+                        <div class="mb-3">
                             <input type="email" class="form-control py-2" placeholder="Your email">
                         </div>
 
                         <div class="mb-3">
                             <input type="password" class="form-control py-2" placeholder="Password">
                         </div>
-                        <button type="submit" class="btn btn-primary d-block col-5 mx-auto my-3">Login</button>
+
+                        <div class="mb-3">
+                            <input type="password" class="form-control py-2" placeholder="Confirm password">
+                        </div>
+                        <button type="submit" class="btn btn-primary d-block col-5 mx-auto my-3">Register</button>
                     </form>
                 </div>
                 <div class="card-footer text-center py-3">
-                    <a href="<?= url_for('dashboard/register.php') ?>">Don't have an account yet?</a>
+                    <a href="<?= url_for('dashboard/login.php') ?>">Already have an account?</a>
                 </div>
             </div>
             <!-- /.card -->
