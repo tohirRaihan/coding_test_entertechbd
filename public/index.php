c92458853e14b5803b9abc6628ecce14ce1579ff
Tohir Raihan <tohir.raihan@gmail.com>
2021-12-17 13:34:48 +0600

    products table created for showing all products to the user

--- public/index.php
+++ public/index.php
@@ -1,18 +1,41 @@
 <?php require_once('../private/initialize.php'); ?>
 
 <!-- #####=START Header=##### -->
-<?php require_once(SHARED_PATH.'/header.php'); ?>
+<?php require_once(SHARED_PATH . '/header.php'); ?>
 <!-- #####=END Header=##### -->
 
 <!-- main content -->
 <div class="container">
     <div class="row">
         <div class="col">
-            <h1 class="mt-5 text-center">Public face of your application !!</h1>
+            <h1 class="mt-5 text-center">All products</h1>
+            <table class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Unit Price</th>
+                        <th scope="col">Location</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Mark</td>
+                        <td>Mark</td>
+                        <td>
+                            <button class="btn btn-sm btn-primary">Buy<i class="fas fa-shopping-basket ms-1"></i></button>
+                        </td>
+                    </tr>
+                </tbody>
+
+            </table>
         </div>
     </div>
 </div>
 
 <!-- #####=START FOOTER=##### -->
-<?php require_once(SHARED_PATH.'/footer.php'); ?>
+<?php require_once(SHARED_PATH . '/footer.php'); ?>
 <!-- #####=END FOOTER=##### -->
