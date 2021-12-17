5aaa1778ad2a6711f9307fea77ede372acedbc02
Tohir Raihan <tohir.raihan@gmail.com>
2021-12-18 04:34:15 +0600

    add method added in Order class

--- private/classes/app/Order.php
+++ private/classes/app/Order.php
@@ -6,6 +6,24 @@ use Database\Database;
 
 class Order extends Database
 {
+    public static function all()
+    {
+        $sql = "SELECT
+                orders.id,
+                orders.quantity,
+                orders.amount,
+                orders.status,
+                users.name as user_name,
+                users.email as user_email,
+                products.name as product_name
+                FROM orders
+                JOIN users
+                ON orders.user_id = users.id
+                JOIN products
+                ON orders.product_id = products.id;";
+        return parent::getRows($sql, []);
+    }
+
     public static function create($user_id, $product_id, $quantity, $amount)
     {
         $sql = "INSERT INTO `orders`(`user_id`, `product_id`, `quantity`, `amount`) VALUES (?,?,?,?)";
