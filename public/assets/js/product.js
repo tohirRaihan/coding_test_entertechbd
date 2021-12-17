70c3b16342df1bbcb37fdebbb80438e0dc9c95d5
Tohir Raihan <tohir.raihan@gmail.com>
2021-12-17 19:39:39 +0600

    increase decrease functionality for product quantity added

--- public/assets/js/product.js
+++ public/assets/js/product.js
@@ -1,3 +1,22 @@
+
+// Increase the order quantity
+const increase = (event) => {
+    const quantityInput = event.target.closest('div').querySelector('input')
+    const quantity = parseInt(quantityInput.value);
+    const newQuantity = quantity + 1;
+    quantityInput.value = newQuantity;
+}
+
+// Decrease the order quantity
+const decrease = (event) => {
+    const quantityInput = event.target.closest('div').querySelector('input')
+    const quantity = parseInt(quantityInput.value);
+    if (quantity > 0) {
+        const newQuantity = quantity - 1;
+        quantityInput.value = newQuantity;
+    }
+}
+
 const buyProduct = (id) => {
 // console.log(id);
     // const confirmBuy = confirm('do you want to buy?');
@@ -7,12 +26,12 @@ const buyProduct = (id) => {
     // }
     Swal.fire({
         title: 'Are you sure?',
-        text: "You won't be able to revert this!",
+        text: "Do you really want to purchase this product?",
         icon: 'question',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
+        confirmButtonText: 'Yes, purchase it!'
       }).then((result) => {
         if (result.isConfirmed) {
         //   Swal.fire(
--- public/index.php
+++ public/index.php
@@ -26,7 +26,6 @@ $scripts = ['product'];
                         <th scope="col">Unit Price</th>
                         <th scope="col">Location</th>
                         <th scope="col">Quantity</th>
-                        <th scope="col">Total Amount</th>
                         <th scope="col">Action</th>
                     </tr>
                 </thead>
@@ -39,12 +38,11 @@ $scripts = ['product'];
                             <td><?= $product['location'] ?></td>
                             <td>
                                 <div class="input-group mx-auto" style="width: 120px;">
-                                    <button class="input-group-text">+</button>
-                                    <input type="text" class="form-control">
-                                    <button class="input-group-text">-</button>
+                                    <button onclick="decrease(event)" class="input-group-text">-</button>
+                                    <input type="text" class="form-control" value="1">
+                                    <button onclick="increase(event)" class="input-group-text">+</button>
                                 </div>
                             </td>
-                            <td>$<?= $product['unit_price'] ?></td>
                             <td>
                                 <button onclick="buyProduct(<?= $product['id'] ?>)" class="btn btn-sm btn-primary">Buy<i class="fas fa-shopping-basket ms-1"></i></button>
                             </td>
