3c5ab01046c9bbb7467e17c3ea638271e4e26b18
Tohir Raihan <tohir.raihan@gmail.com>
2021-12-17 20:43:58 +0600

    set all variables in new order data to create an order

--- public/data/orders/new_order.php
+++ public/data/orders/new_order.php
@@ -1,27 +1,30 @@
 <?php
+
+use App\Product;
+use Database\Session;
+
 require_once '../../../private/initialize.php';
 
 
 $response = file_get_contents('php://input');
 $data     = json_decode($response, true);
-print_r($data);
-die;
-// reciving all data
+
+// check if user is logged in or not
+if (!Session::getSessionData('user_logged')) {
+    $return['status'] = 'userNotLoggedIn';
+    echo json_encode($return);
+    die;
+}
+
+// reciving all data and calculation
 $product_id = $data['productId'];
-$order_quantity = $data['orderQuantity'];
+$product = Product::find($product_id);
+$unit_price = $product['unit_price'];
+$quantity = $data['orderQuantity'];
+$amount = $unit_price * $quantity;
+$user_id = Session::getSessionData('user_logged');
 
-// // Calculate total ordered amount;
-// $ordered_amount = 0;
-// foreach ($ordered_items as $order_item) {
-//     $item             = Item::find($order_item['itemId']);
-//     $item_quantity    = $order_item['itemQuantity'];
-//     $item_price       = $item['price'];
-//     $item_total_price = $item_price * $item_quantity;
-//     $ordered_amount  += $item_total_price;
-// }
-// // send the ordered items as json file
-// $ordered_items = json_encode($ordered_items);
-
+// // create new order
 // $create_new_order = Order::create($customer_name, $ordered_items, $ordered_amount);
 // if ($create_new_order) {
 //     $return['status'] = 'success';
